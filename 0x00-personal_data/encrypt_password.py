b75057c6740e86f20bdb5003d53009c2da477464
Drihmia <drihmia.redouane@gmail.com>
2024-07-04 00:19:11 +0100

    feat: Add task 6, implemented a is_valid function checks if a pwd matches the hashed pwd

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -3,8 +3,23 @@
 import bcrypt
 
 
+# Task 5:
 def hash_password(password: str) -> bytes:
     """ Hash a password for the first time """
+
     salt = bcrypt.gensalt()
-    hashed_password = bcrypt.hashpw(b'password', salt)
+
+    hashed_password = bcrypt.hashpw(
+        password.encode(),
+        salt)
+
     return hashed_password
+
+
+# Task 6:
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """ Check if the password is valid """
+
+    return bcrypt.checkpw(
+        password.encode(),
+        hashed_password)
