2a690a7c09c7cde7adee004dd718ce8a94e181eb
Drihmia <drihmia.redouane@gmail.com>
2024-07-08 21:57:45 +0100

    chore: change back the type check returned by handlers

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -4,10 +4,8 @@ Route module for the API
 """
 from os import getenv
 from api.v1.views import app_views
-from flask import Flask, jsonify, abort, request, Response
+from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
-import os
-from typing import Tuple, Dict
 
 
 app = Flask(__name__)
@@ -26,21 +24,21 @@ elif auth_env == "basic_auth":
 
 
 @app.errorhandler(404)
-def not_found(error) -> Tuple[Response, int]:
+def not_found(error) -> str:
     """ Not found handler
     """
     return jsonify({"error": "Not found"}), 404
 
 
 @app.errorhandler(401)
-def unauthorized(error) -> Tuple[Response, int]:
+def unauthorized(error) -> str:
     """Not authorized requests
     """
     return jsonify({"error": "Unauthorized"}), 401
 
 
 @app.errorhandler(403)
-def forbidden(error) -> Tuple[Response, int]:
+def forbidden(error) -> str:
     """ Not allowed requests
     """
     return jsonify({
