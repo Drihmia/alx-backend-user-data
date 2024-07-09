aa2cbe344faf07a15fde847716bb28b30a4eac9c
Drihmia <drihmia.redouane@gmail.com>
2024-07-09 13:41:41 +0100

    fix: rewrite the type check of extract_user_credentials to return (str, str) instead of Tuple[str, str)]

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,8 +1,43 @@
 #!/usr/bin/env python3
 """ Basic Auth """
 from api.v1.auth.auth import Auth
+import base64
+import binascii
 
 
 class BasicAuth(Auth):
     """ Basic Auth inherite from Auth class.
     """
+
+    def extract_base64_authorization_header(self, authorization_header: str) -> str:
+        """ A method that returns the Base64 part of the Authorization
+        header for a Basic Authentication.
+        """
+        if not authorization_header:
+            return None
+
+        if type(authorization_header) is not str:
+            return None
+
+        if not authorization_header.startswith("Basic "):
+            return None
+
+        auth_arr = authorization_header.split(" ")
+        if len(auth_arr) > 1:
+            return auth_arr[1]
+        return None
+
+    def decode_base64_authorization_header(self, base64_authorization_header: str) -> str:
+        """ A method that returns the decoded value of a Base64 string.
+        """
+        if not base64_authorization_header:
+            return None
+
+        if type(base64_authorization_header) is not str:
+            return None
+
+        try:
+            decoded_auth_head = base64.b64decode(base64_authorization_header)
+            return decoded_auth_head.decode('utf-8')
+        except (binascii.Error, UnicodeDecodeError):
+            return None
--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # alx-backend-user-data
 alx-backend-user-data
+
+
+
+
+
+# License
+
+This project is licensed under the GNU Affero General Public License v3.0 (AGPL-3.0). See the [LICENSE](LICENSE) file for details.
+
+**Note:** This code is provided for educational purposes only and should not be used to complete assignments or projects in any academic context where similar solutions are being evaluated. Any such misuse is strictly prohibited.
+
