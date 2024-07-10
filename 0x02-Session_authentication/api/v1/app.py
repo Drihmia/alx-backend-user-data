45827b7a8c2aaddd8bae4d4102f39247be95b938
Drihmia <drihmia.redouane@gmail.com>
2024-07-10 23:10:21 +0100

    feat: init commit on 0x02-Session_authentication+ task 0

---
+++ 0x02-Session_authentication/api/v1/app.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python3
+"""
+Route module for the API
+"""
+from os import getenv
+from api.v1.views import app_views
+from flask import Flask, jsonify, abort, request
+from flask_cors import (CORS, cross_origin)
+
+
+app = Flask(__name__)
+app.register_blueprint(app_views)
+CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
+
+auth = None
+auth_env = getenv("AUTH_TYPE")
+
+if auth_env == "auth":
+    from api.v1.auth.auth import Auth
+    auth = Auth()
+elif auth_env == "basic_auth":
+    from api.v1.auth.basic_auth import BasicAuth
+    auth = BasicAuth()
+
+
+@app.errorhandler(404)
+def not_found(error) -> str:
+    """ Not found handler
+    """
+    return jsonify({"error": "Not found"}), 404
+
+
+@app.errorhandler(401)
+def unauthorized(error) -> str:
+    """Not authorized requests
+    """
+    return jsonify({"error": "Unauthorized"}), 401
+
+
+@app.errorhandler(403)
+def forbidden(error) -> str:
+    """ Not allowed requests
+    """
+    return jsonify({
+        "error": "Forbidden"
+    }), 403
+
+
+@app.before_request
+def filter():
+    """ Filter incoming requests.
+    """
+    if not auth:
+        return
+
+    exc_list = [
+        '/api/v1/status/',
+        '/api/v1/unauthorized/',
+        '/api/v1/forbidden/'
+    ]
+
+    path = str(request.path)
+
+    if not auth.require_auth(path, exc_list):
+        return
+
+    if not auth.authorization_header(request):
+        abort(401)
+
+    if not auth.current_user(request):
+        abort(403)
+    request.current_user = auth.current_user(request)
+
+
+if __name__ == "__main__":
+    host = getenv("API_HOST", "0.0.0.0")
+    port = getenv("API_PORT", "5000")
+    app.run(host=host, port=port)
---
+++ 0x02-Session_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,115 @@
+#!/usr/bin/env python3
+""" Basic Auth """
+import base64
+import binascii
+from typing import TypeVar
+from api.v1.auth.auth import Auth
+from models.user import User
+
+
+class BasicAuth(Auth):
+    """ Basic Auth inherite from Auth class.
+    """
+
+    def extract_base64_authorization_header(self, authorization_header: str
+                                            ) -> str:
+        """ A method that returns the Base64 part of the Authorization
+        header for a Basic Authentication.
+        """
+        if not authorization_header:
+            return None
+
+        if type(authorization_header) is not str:
+            return None
+
+        if not authorization_header.startswith("Basic "):
+            return None
+
+        auth_arr = authorization_header.split(" ")
+        if len(auth_arr) > 1:
+            return auth_arr[1]
+        return None
+
+    def decode_base64_authorization_header(self,
+                                           base64_authorization_header: str
+                                           ) -> str:
+        """ A method that returns the decoded value of a Base64 string.
+        """
+        if not base64_authorization_header:
+            return None
+
+        if type(base64_authorization_header) is not str:
+            return None
+
+        try:
+            decoded_auth_head = base64.b64decode(base64_authorization_header)
+            return decoded_auth_head.decode('utf-8')
+        except (binascii.Error, UnicodeDecodeError):
+            return None
+
+    def extract_user_credentials(self, decoded_base64_authorization_header: str
+                                 ) -> (str, str):
+        """ A method that returns the user email and password from the Base64
+        decoded value.
+        """
+        if not decoded_base64_authorization_header:
+            return None, None
+
+        if type(decoded_base64_authorization_header) is not str:
+            return None, None
+
+        if ":" not in decoded_base64_authorization_header:
+            return None, None
+
+        credentials = decoded_base64_authorization_header.split(
+            ":", maxsplit=1)
+
+        try:
+            return credentials[0], credentials[1]
+        except IndexError:
+            return None, None
+
+    def user_object_from_credentials(self, user_email: str, user_pwd: str
+                                     ) -> TypeVar('User'):
+        """ A method that returns the User instance based on his email and
+        password.
+        """
+        if not user_email or type(user_email) is not str:
+            return None
+
+        if not user_pwd or type(user_pwd) is not str:
+            return None
+        try:
+            user_list = User.search({"email": user_email})
+        except Exception as e:
+            return None
+        user_list = list(filter(
+            lambda user: user.is_valid_password(user_pwd), user_list))
+
+        if not len(user_list):
+            return None
+
+        user = user_list[0]
+        return user
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """ A method that overloads Auth and retrieves the User instance for
+        a request.
+        """
+        auth_body = super().authorization_header(request)
+
+        if not auth_body:
+            return None
+
+        auth_token = self.extract_base64_authorization_header(auth_body)
+        if not auth_token:
+            return None
+
+        decoded_token = self.decode_base64_authorization_header(auth_token)
+        if not decoded_token:
+            return None
+
+        user_email, user_pwd = self.extract_user_credentials(decoded_token)
+        if not user_email or not user_pwd:
+            return None
+        return self.user_object_from_credentials(user_email, user_pwd)
